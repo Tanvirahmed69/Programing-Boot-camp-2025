5e2b5fdd1579e3aa4fbca1d513ab99a3367ba030
tanvirahmed69 <ahmedtanveer004@gmail.com>
2025-10-12 16:31:13 +0300

    Take a number flot , int

---
+++ 1.py
@@ -0,0 +1,10 @@
+# Add two numbers and print the result
+
+
+
+a =float(input("give your fist number "))
+b=float(input("give your secound number"))
+c=float(input("give your third number"))
+print(f"the result is:{a+b+c}") 
+
+
