5e16b40f3484fbea9fb214f5cee58f9552068f97
tanvirahmed69 <ahmedtanveer004@gmail.com>
2025-10-12 17:42:46 +0300

    #Print the type of 42

---
+++ 3.py
@@ -0,0 +1,6 @@
+#Print your name.....
+
+print("my name is XYZ")
+a=input("what is your First name: ")
+b=input("what is your last name: ")
+print(f"full name {a+''+b}")
--- 4.py
+++
@@ -1,6 +0,0 @@
-#Print your name.....
-
-print("my name is XYZ")
-a=input("what is your First name: ")
-b=input("what is your last name: ")
-print(f"full name {a+''+b}")
