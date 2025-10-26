d144796c8f039acc2f52755044e5a9da2805aa9d
tanvirahmed69 <ahmedtanveer004@gmail.com>
2025-10-26 17:52:01 +0200

    Grade system: A(>=90), B(>=80), C(>=70), D(>=60), F

---
+++ Day 3/1.M.py
@@ -0,0 +1,14 @@
+##Grade system: A(>=90), B(>=80), C(>=70), D(>=60), F
+
+grade=float(input('what is your result?'))
+                  
+if grade >=90:
+    print('A')
+elif grade >=80:
+    print('B')
+elif grade >=70:
+    print('C')
+elif grade >=60:
+    print('D')
+else:
+    print('F')
