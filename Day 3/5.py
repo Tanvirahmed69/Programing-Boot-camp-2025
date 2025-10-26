e8c8278a41065a5972fbf9c00ea1c74719402d13
tanvirahmed69 <ahmedtanveer004@gmail.com>
2025-10-26 17:24:13 +0200

    Check if char is vowel (a, e, i, o, u)

--- Day 3/5.py
+++ Day 3/5.py
@@ -1,12 +1,10 @@
 ###Check if char is vowel (a, e, i, o, u)
 
 
-name='tanvir'
+name='a'
 
-        if name in ['a', 'e', 'i', 'o', 'u']:
-                print( 'is a vowel')
-        
-        
+if name in ['a', 'e', 'i', 'o', 'u']:
+    print( 'is a vowel')     
 else:
-        print( 'is a consonant')
+    print( 'is a consonant')
         
