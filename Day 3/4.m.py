cc8cadfc453cb84c4c2a7cd4330815bda6d229a1
tanvirahmed69 <ahmedtanveer004@gmail.com>
2025-11-02 15:27:31 +0200

    4tr

--- Day 3/4.m.py
+++ Day 3/4.m.py
@@ -1,2 +1,10 @@
 ##Check if triangle is valid (sum of any two > third)
 
+a=float(input('gime  your 1st number:'))
+b=float(input('give your 2nd number'))
+c=float(input('give tour 3rd number'))
+
+if a=b>c and b<c and c=a+b:
+    print('Tui besi bujos')
+else:
+    print('Pagol')
